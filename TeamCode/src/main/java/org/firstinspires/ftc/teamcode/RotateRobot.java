e0829c82c6587efde0a701032625b95d3a745fd9
AndyL07 <andrew.lullo5@gmail.com>
2024-10-15 15:57:09 -0500

    Rotate Robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateRobot.java
@@ -1,27 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.sun.tools.javac.jvm.Gen;
+
+import org.firstinspires.ftc.robotcore.external.matrices.ColumnMatrixF;
+import org.firstinspires.ftc.robotcore.external.matrices.DenseMatrixF;
+import org.firstinspires.ftc.robotcore.external.matrices.GeneralMatrixF;
 import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 
 public class RotateRobot
 {
+    float angle;
+    VectorF relativeRobot;
+    GeneralMatrixF rotationMatrix = new GeneralMatrixF(3,3);
+
+
     public RotateRobot (float yaw, VectorF relativePosition)
     {
-        MatrixF rotation = new MatrixF(3, 3) {
-            @Override
-            public MatrixF emptyMatrix(int numRows, int numCols) {
-                return null;
-            }
+        angle = yaw;
+        relativeRobot = relativePosition;
 
-            @Override
-            public float get(int row, int col) {
-                return 0;
-            }
+        rotationMatrix.put(1,1,(float) Math.cos(angle));
+        rotationMatrix.put(2,1,(float) Math.sin(angle));
+        rotationMatrix.put(3,1,(float) 0);
 
-            @Override
-            public void put(int row, int col, float value) {
-                
-            }
-        };
+        rotationMatrix.put(1,2,(float) -Math.sin(angle));
+        rotationMatrix.put(2,2,(float) Math.cos(angle));
+        rotationMatrix.put(3,2,(float) 0);
+
+        rotationMatrix.put(1,3,(float) 0);
+        rotationMatrix.put(2,3,(float) 0);
+        rotationMatrix.put(3,3,(float) 1);
+
+
+        rotationMatrix.multiplied(relativePosition);
+    }
+
+    public void changeAngle(float yaw)
+    {
+        angle = yaw;
+    }
+
+    public VectorF getAbsoluteRobot() {
+        return rotationMatrix.multiplied(relativeRobot);
     }
 }
