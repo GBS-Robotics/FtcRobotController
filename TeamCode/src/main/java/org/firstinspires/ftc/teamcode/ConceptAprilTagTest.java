64b0047e7ae510c9511ef513767d8062ab4e3d2a
AndyL07 <andrew.lullo5@gmail.com>
2024-10-15 17:15:06 -0500

    Proper Robot Pose Calculation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagTest.java
@@ -36,6 +36,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
@@ -82,8 +83,11 @@ public class ConceptAprilTagTest extends LinearOpMode {
      */
     private VisionPortal visionPortal;
 
-    private org.firstinspires.ftc.robotcore.external.matrices.VectorF aprilTagDistance = new VectorF(0.0f, 0.0f, 0.0f);
-    private org.firstinspires.ftc.robotcore.external.matrices.VectorF robotPose = new VectorF(0.0f, 0.0f, 0.0f);
+    private VectorF aprilTagDistance = new VectorF(0.0f, 0.0f, 0.0f);
+    private VectorF robotPose = new VectorF(0.0f, 0.0f, 0.0f);
+    Quaternion q = new Quaternion();
+    EulerAngle angles = new EulerAngle(q);
+    RotateRobot relativeField = new RotateRobot(0, aprilTagDistance);
 
     @Override
     public void runOpMode() {
@@ -204,18 +208,23 @@ public class ConceptAprilTagTest extends LinearOpMode {
         // Step through the list of detections and display info for each one.
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
-                EulerAngle angles = new EulerAngle(detection.metadata.fieldOrientation);
+                angles.setQuaternion(detection.metadata.fieldOrientation);
 
                 telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
                 telemetry.addLine(String.format("Field Position: %s", detection.metadata.fieldPosition.toString()));
-//                telemetry.addLine(String.format("Field Orientation: %s", detection.metadata.fieldOrientation.toString()));
+                //telemetry.addLine(String.format("Field Orientation: %s", detection.metadata.fieldOrientation.toString()));
                 telemetry.addLine(String.format("Euler Angles: %s", angles.toString()));
                 telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                 telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                 telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
 
-                aprilTagDistance = new VectorF((float) detection.ftcPose.x, (float) detection.ftcPose.y, (float) detection.ftcPose.z);
-                robotPose = detection.metadata.fieldPosition.subtracted(aprilTagDistance);
+                aprilTagDistance.put(1,(float) detection.ftcPose.x);
+                aprilTagDistance.put(2,(float) detection.ftcPose.y);
+                aprilTagDistance.put(3,(float) detection.ftcPose.z);
+
+                relativeField.updateInfo(angles.yaw,aprilTagDistance);
+
+                robotPose = detection.metadata.fieldPosition.subtracted(relativeField.getRelativeField());
                 telemetry.addLine(String.format("Robot Position: %s", robotPose.toString()));
             } else {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotateRobot.java
@@ -1,24 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.sun.tools.javac.jvm.Gen;
-
-import org.firstinspires.ftc.robotcore.external.matrices.ColumnMatrixF;
-import org.firstinspires.ftc.robotcore.external.matrices.DenseMatrixF;
 import org.firstinspires.ftc.robotcore.external.matrices.GeneralMatrixF;
-import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 
 public class RotateRobot
 {
     float angle;
-    VectorF relativeRobot;
+    VectorF relativeField;
     GeneralMatrixF rotationMatrix = new GeneralMatrixF(3,3);
 
 
-    public RotateRobot (float yaw, VectorF relativePosition)
+    public RotateRobot(float yaw, VectorF relativePosition)
     {
         angle = yaw;
-        relativeRobot = relativePosition;
+        relativeField = relativePosition;
 
         rotationMatrix.put(1,1,(float) Math.cos(angle));
         rotationMatrix.put(2,1,(float) Math.sin(angle));
@@ -31,17 +26,28 @@ public class RotateRobot
         rotationMatrix.put(1,3,(float) 0);
         rotationMatrix.put(2,3,(float) 0);
         rotationMatrix.put(3,3,(float) 1);
-
-
-        rotationMatrix.multiplied(relativePosition);
     }
 
-    public void changeAngle(float yaw)
+    public void updateInfo(float yaw, VectorF relativePosition)
     {
         angle = yaw;
+        relativeField = relativePosition;
+
+        rotationMatrix.put(1,1,(float) Math.cos(angle));
+        rotationMatrix.put(2,1,(float) Math.sin(angle));
+        rotationMatrix.put(3,1,(float) 0);
+
+        rotationMatrix.put(1,2,(float) -Math.sin(angle));
+        rotationMatrix.put(2,2,(float) Math.cos(angle));
+        rotationMatrix.put(3,2,(float) 0);
+
+        rotationMatrix.put(1,3,(float) 0);
+        rotationMatrix.put(2,3,(float) 0);
+        rotationMatrix.put(3,3,(float) 1);
     }
 
-    public VectorF getAbsoluteRobot() {
-        return rotationMatrix.multiplied(relativeRobot);
+    public VectorF getRelativeField()
+    {
+        return rotationMatrix.multiplied(relativeField);
     }
 }
