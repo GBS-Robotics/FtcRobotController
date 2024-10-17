b6700ccdaf813409185b1ab24afbc32db395dcd9
GBS-Robotics <gbs_robotics@outlook.com>
2024-10-17 16:25:38 -0500

    Servo fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoArmTest.java
@@ -1,51 +1,57 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TestArmServos", group = "Practice")
 public class ServoArmTest extends LinearOpMode {
 
-    static final double INCREMENT = 0.01;
-    static final int CYCLE_MS = 50;
-    static final double MAX_POS = 1.0;
-    static final double MIN_POS = 0;
+    static final int CYCLE_MS = 10;
 
-    public static double ARM_HAND_LEFT_POWER = 0;
-    public static double ARM_HAND_RIGHT_POWER = 0;
 
     @Override
     public void runOpMode() {
 
-        CRServo armHandLeft = hardwareMap.get(CRServo.class, "arm_hand_left");
-        CRServo armHandRight = hardwareMap.get(CRServo.class, "arm_hand_right");
-        CRServo armBase = hardwareMap.get(CRServo.class, "arm_base");
+        Servo armHandLeft = hardwareMap.get(Servo.class, "arm_hand_left");
+        Servo armHandRight = hardwareMap.get(Servo.class, "arm_hand_right");
+        DcMotor armBase = hardwareMap.get(DcMotor.class, "arm_base");
+        double right = 0;
+        double left = 0;
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            if (ARM_HAND_LEFT_POWER > MAX_POS) {
-                ARM_HAND_LEFT_POWER -= INCREMENT;
-                armHandLeft.setPower(ARM_HAND_LEFT_POWER);
-                armHandRight.setPower(-ARM_HAND_LEFT_POWER);
-            } else if (ARM_HAND_LEFT_POWER < MIN_POS) {
-                ARM_HAND_LEFT_POWER += INCREMENT;
-                armHandLeft.setPower(ARM_HAND_LEFT_POWER);
-                armHandRight.setPower(-ARM_HAND_LEFT_POWER);
-            } else {
-                ARM_HAND_LEFT_POWER = 0;
+            //open claw
+            if (gamepad2.left_bumper)
+            {
+                left = 1;
+                right = 0;
             }
 
-            sleep(CYCLE_MS);
+            //close claw
+            if (gamepad2.right_bumper)
+            {
+                left = 0;
+                right = 1;
+            }
+
+            armHandRight.setPosition(right);
+            armHandLeft.setPosition(left);
+
+            telemetry.addData(">", "Done");
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", left, right);
+            telemetry.update();
+
             idle();
         }
 
         // Signal done;
-        telemetry.addData(">", "Done");
-        telemetry.update();
     }
 
 }
