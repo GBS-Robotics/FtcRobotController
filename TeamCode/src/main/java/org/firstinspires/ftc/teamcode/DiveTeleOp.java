39aac8678aa73a7123f58711e4e77a27cb95db02
GBS-Robotics <gbs_robotics@outlook.com>
2024-10-24 18:08:45 -0500

    Drive Working (mostly)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiveTeleOp.java
@@ -2,78 +2,62 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-//This class is to be used as a running example of what a basic op mode looks like
-//This class should not be changed in any way
-//This class is meant to be a way to understand possible strategies for creating our own op mode
-
-//This is a test for pull requests
 @TeleOp(name="DiveTeleOp")
 public class DiveTeleOp extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
-    private CRServo grabberServo = null;
+    private final ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
 
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-        grabberServo = hardwareMap.get(CRServo.class, "grabberServo");
+        // Base motors
+        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        // Arm servos and motors
+        Servo armHandLeft = hardwareMap.get(Servo.class, "arm_hand_left");
+        Servo armHandRight = hardwareMap.get(Servo.class, "arm_hand_right");
+        DcMotor armBase = hardwareMap.get(DcMotor.class, "arm_base");
 
-        // ########################################################################################
-        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-        // ########################################################################################
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // Wait for the game to start (driver presses START)
+        double axial;
+        double lateral;
+        double yaw;
+        double armSpeed;
+
+        double arm_left = 0;
+        double arm_right = 0;
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             double max;
 
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
-            double speed = gamepad2.right_stick_y;
+            axial   = -gamepad1.left_stick_y;
+            lateral =  gamepad1.left_stick_x;
+            yaw     =  gamepad1.right_stick_x;
+            armSpeed = gamepad2.right_stick_y;
 
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
             double leftFrontPower  = axial + lateral + yaw;
             double rightFrontPower = axial - lateral - yaw;
             double leftBackPower   = axial - lateral + yaw;
             double rightBackPower  = axial + lateral - yaw;
 
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
             max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
@@ -83,38 +67,41 @@ public class DiveTeleOp extends LinearOpMode {
                 rightFrontPower /= max;
                 leftBackPower   /= max;
                 rightBackPower  /= max;
-                speed /= max;
+                armSpeed /= max;
             }
 
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
+            if (gamepad2.right_bumper) {
+                arm_left = 1;
+                arm_right = 0;
+            }
 
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
+            if (gamepad2.left_bumper) {
+                arm_left = 0;
+                arm_right = 1;
+            }
+
+            if (gamepad2.right_trigger > 0.1) {
+                armBase.setPower(gamepad2.right_trigger);
+            }
+
+            if (gamepad2.left_trigger > 0.1) {
+                armBase.setPower(-gamepad2.left_trigger);
+            }
 
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
             rightFrontDrive.setPower(rightFrontPower);
             leftBackDrive.setPower(leftBackPower);
             rightBackDrive.setPower(rightBackPower);
-            grabberServo.setPower(speed);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Servo", "%4.2f", speed);
+            armHandLeft.setPosition(arm_left);
+            armHandRight.setPosition(arm_right);
+
+            telemetry.addData("Status", "Run Time: " + runtime);
+            telemetry.addData("Front left/right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back left/right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("Hand left/right", "%4.2f, %4.2f", arm_left, arm_right);
+            telemetry.addData("Servo", "%4.2f", armSpeed);
             telemetry.update();
         }
     }}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoArmTest.java
@@ -2,45 +2,50 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TestArmServos", group = "Practice")
 public class ServoArmTest extends LinearOpMode {
 
-    static final double INCREMENT = 0.01;
-    static final int CYCLE_MS = 50;
-    static final double MAX_POS = 1.0;
-    static final double MIN_POS = 0;
-
-    public static double ARM_HAND_LEFT_POWER = 0;
-    public static double ARM_HAND_RIGHT_POWER = 0;
+    public static double arm_left = 0;
+    public static double arm_right = 0;
 
     @Override
     public void runOpMode() {
 
-        CRServo armHandLeft = hardwareMap.get(CRServo.class, "arm_hand_left");
-        CRServo armHandRight = hardwareMap.get(CRServo.class, "arm_hand_right");
-        CRServo armBase = hardwareMap.get(CRServo.class, "arm_base");
+        Servo armHandLeft = hardwareMap.get(Servo.class, "arm_hand_left");
+        Servo armHandRight = hardwareMap.get(Servo.class, "arm_hand_right");
+        DcMotor armBase = hardwareMap.get(DcMotor.class, "arm_base");
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            if (ARM_HAND_LEFT_POWER > MAX_POS) {
-                ARM_HAND_LEFT_POWER -= INCREMENT;
-                armHandLeft.setPower(ARM_HAND_LEFT_POWER);
-                armHandRight.setPower(-ARM_HAND_LEFT_POWER);
-            } else if (ARM_HAND_LEFT_POWER < MIN_POS) {
-                ARM_HAND_LEFT_POWER += INCREMENT;
-                armHandLeft.setPower(ARM_HAND_LEFT_POWER);
-                armHandRight.setPower(-ARM_HAND_LEFT_POWER);
-            } else {
-                ARM_HAND_LEFT_POWER = 0;
+            if (gamepad2.right_bumper) {
+                arm_left = 0;
+                arm_right = 1;
             }
 
-            sleep(CYCLE_MS);
-            idle();
+            if (gamepad2.left_bumper) {
+                arm_left = 1;
+                arm_right = 0;
+            }
+
+            if (gamepad2.right_trigger > 0.1) {
+                armBase.setPower(gamepad2.right_trigger);
+            }
+
+            if (gamepad2.left_trigger > 0.1) {
+                armBase.setPower(-gamepad2.left_trigger);
+            }
+
+            armHandLeft.setPosition(arm_left);
+            armHandRight.setPosition(arm_right);
+
+            telemetry.addData("Left/Right", "%4.2f, %4.2f", arm_left, arm_right);
+            telemetry.update();
         }
 
         // Signal done;
